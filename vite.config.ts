1f80c71a0209dde99d1289f2110cd95382555306
Sean McCart <seanmccart@gmail.com>
2026-02-27 14:04:26 -0500

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,9 @@ import path from 'path'
 
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  server: {
+    port: 5184,
+  },
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
